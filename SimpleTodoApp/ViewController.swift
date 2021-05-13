2ca1b468638ed123c8cc2465968c06173256237c
Jamie <jamie.yjchen@gmail.com>
2021-05-12 19:18:08 -0700

    init Project

---
+++ SimpleTodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTodoApp
+//
+//  Created by Jamie Chen on 2021-05-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
